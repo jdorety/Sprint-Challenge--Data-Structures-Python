347e985fb5a5b2e51572cb142c25aba6d3feb714
Jonathan Dorety <kyullog@gmail.com>
2019-09-14 10:28:55 -0700

    simple optimization (not valid, I think)

--- names/names.py
+++ names/names.py
@@ -1,6 +1,6 @@
 import time
 
-start_time = time.time()
+start_old = time.time()
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
@@ -16,7 +16,26 @@ for name_1 in names_1:
         if name_1 == name_2:
             duplicates.append(name_1)
 
-end_time = time.time()
+end_old = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print (f"runtime: {end_old - start_old} seconds")
 
+
+start_new = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates = []
+for name_1 in names_1:
+    if name_1 in names_2:
+        duplicates.append(name_1)
+
+end_new = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_new - start_new} seconds")
