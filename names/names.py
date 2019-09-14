6a34a00942298ed5477064742bf427d2a5376b9c
Jonathan Dorety <kyullog@gmail.com>
2019-09-14 12:00:10 -0700

    Noon o'clock commit

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from doubly_linked_list import DoublyLinkedList
 
 start_old = time.time()
 
@@ -23,18 +24,34 @@ print (f"runtime: {end_old - start_old} seconds")
 
 start_new = time.time()
 
-f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
-f.close()
+def make_DLL(arr):
+    new_DLL = DoublyLinkedList()
+    for i in arr:
+        new_DLL.add_to_tail(i)
+    return new_DLL
 
-f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
-f.close()
-
+new_names_1 = make_DLL(names_1)
+new_names_2 = make_DLL(names_2)
 duplicates = []
-for name_1 in names_1:
-    if name_1 in names_2:
-        duplicates.append(name_1)
+
+node_1 = new_names_1.head
+node_2 = new_names_2.head
+
+for i in range(new_names_1.length):
+    for j in range(new_names_2.length):
+        if node_1.value is node_2.value:
+            duplicates.append(node_1.value)
+        if node_2.next is None:
+            break
+    if node_1.next is None:
+        break
+    node_1 = node_1.next
+
+
+
+
+
+
 
 end_new = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
